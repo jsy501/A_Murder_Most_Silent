ecd2555467ce1570ef1c8d353826cf3b6c642eb8
Unknown <jsy501@york.ac.uk>
2016-11-12 19:58:58 +0000

    Create project

---
+++ desktop/src/com/noname/mrch/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.noname.mrch.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.noname.mrch.MRCH;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MRCH(), config);
+	}
+}
