f9ffa15fa7ce3b24422185b2f874c0a1411d4f69
Jeff <jsy501@york.ac.uk>
2016-11-24 10:30:48 +0000

    Class creation

---
+++ core/src/com/noname/mrch/NoteBook.java
@@ -0,0 +1,16 @@
+package com.noname.mrch;
+
+import com.badlogic.gdx.utils.Array;
+
+public class NoteBook {
+    private Array<Clue> clueList;
+    private Array<Item> itemList;
+
+    public Array<Clue> getClueList() {
+        return clueList;
+    }
+
+    public Array<Item> getItemList() {
+        return itemList;
+    }
+}
