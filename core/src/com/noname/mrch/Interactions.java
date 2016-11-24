155e2531be6f35a32625d0b8c3d572bee9f5a04a
jpn506 <jpn506@york.ac.uk>
2016-11-24 12:07:20 +0000

    interactions skeleton

---
+++ core/src/com/noname/mrch/Interactions.java
@@ -0,0 +1,22 @@
+package com.noname.mrch;
+
+/**
+ * Created by User on 24/11/2016.
+ */
+public class Interactions {
+    String question(int questionStyle,Character target){
+        // does some stuff and the player gets a clue
+    }
+
+    String give(Item item, Character target){
+        // does some stuff and you get some stuff
+    }
+
+    String accuse(Character target){
+        //does some stuff
+    }
+
+    void Ignore(){
+        //exits interaction loop
+    }
+}
