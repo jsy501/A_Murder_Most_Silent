357edfa459ca5df5779960895b826098cf846519
Jeff <jsy501@york.ac.uk>
2016-11-24 17:57:21 +0000

    file directory change

--- core/src/com/noname/mrch/GameWorld.java
+++
@@ -1,9 +0,0 @@
-package com.noname.mrch;
-
-import com.badlogic.gdx.utils.Array;
-
-public class GameWorld {
-    private Array<Item> totalItemList = new Array<Item>();
-    private Array<Clue> totalClueList = new Array<Clue>();
-    private Array<Character> totalCharacterList = new Array<Character>();
-}
--- core/src/com/noname/mrch/Personality.java
+++
@@ -1,7 +0,0 @@
-package com.noname.mrch;
-
-public final class Personality {
-    public static final int FRIENDLY = 0;
-    public static final int NEUTRAL = 1;
-    public static final int AGGRESSIVE = 2;
-}
