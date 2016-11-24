e6e9369e9915279997a331ad9fb82e589d609477
td736 <td736@york.ac.uk>
2016-11-24 11:48:35 +0000

    Char and Room now inherit from HasObject. Changed public variables to private.

--- core/src/com/noname/mrch/HasObject.java
+++ core/src/com/noname/mrch/HasObject.java
@@ -2,11 +2,11 @@ package com.noname.mrch;
 
 import java.util.ArrayList;
 
-public class HasObject {
+private class HasObject {
 	
 	
-	public ArrayList<Item> itemList = new ArrayList<Item>() ;
-	public ArrayList<Clue> clueList = new ArrayList<Clue>() ;
+	private ArrayList<Item> itemList = new ArrayList<Item>() ;
+	private ArrayList<Clue> clueList = new ArrayList<Clue>() ;
 	
 	public void removeClue(Clue clue){
 		this.clueList.remove(clue);
