f2ca4f0377f52e0e06fa5218ef2f198a22069647
jpn506 <jpn506@york.ac.uk>
2016-11-24 12:13:05 +0000

    tests

---
+++ core/src/test/java/com/noname/mrch/PlayerTest.java
@@ -0,0 +1,40 @@
+package com.noname.mrch;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Player Tests
+ */
+public class PlayerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setLocation() throws Exception {
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+    @Test
+    public void getPersonality() throws Exception {
+
+    }
+
+    @Test
+    public void getLocation() throws Exception {
+
+    }
+
+}
