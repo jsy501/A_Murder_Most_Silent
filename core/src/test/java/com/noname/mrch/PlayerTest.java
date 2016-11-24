e7202e86482efd55f5d6024911f57fad93b23f56
jpn506 <jpn506@york.ac.uk>
2016-11-24 12:40:43 +0000

    Player Tests

--- core/src/test/java/com/noname/mrch/PlayerTest.java
+++ core/src/test/java/com/noname/mrch/PlayerTest.java
@@ -10,6 +10,8 @@ import static org.junit.Assert.*;
  * Player Tests
  */
 public class PlayerTest {
+    Player player = new Player("test", 0);
+
     @Before
     public void setUp() throws Exception {
     }
@@ -20,21 +22,44 @@ public class PlayerTest {
 
     @Test
     public void setLocation() throws Exception {
+        // create player
+        // check location
+        // player.setLocation()
+        // check new location
+        // asserts
+        Room testroom = new Room(0,"Test Room",false);
+
+        Room expected = testroom;
+        Room actual = player.getLocation();
+        assertEquals("failure - player location not correct", expected, actual);
+
+
+
     }
 
     @Test
     public void getName() throws Exception {
-
+        String expected = "test";
+        String actual = player.getName();
+        assertEquals("failure - player name not correct",expected,actual);
     }
 
     @Test
     public void getPersonality() throws Exception {
+        int expected = 0;
+        int actual = player.getPersonality();
+
+        assertEquals("failure - players personality is not correct", expected, actual);
 
     }
 
     @Test
     public void getLocation() throws Exception {
+        Room testroom = new Room(0, "test room", false);
+        player.setLocation(testroom);
 
+        Room expected = testroom;
+        Room actual = player.getLocation();
+        assertEquals("failure - room is not the room the player is in", expected, actual);
     }
-
 }
