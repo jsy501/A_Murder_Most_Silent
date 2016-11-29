61caabfc106d5193d059c9c192ddcec1e13f288c
jpn506 <jpn506@york.ac.uk>
2016-11-29 14:33:31 +0000

    interactions accuse test added and works

--- core/src/test/java/com/noname/mrch/InteractionsTest.java
+++ core/src/test/java/com/noname/mrch/InteractionsTest.java
@@ -19,10 +19,12 @@ public class InteractionsTest {
     Clue clueOne;
     Clue clueTwo;
     Clue clueThree;
+    Clue failClue;
     Item item;
     Item returnItem;
 
     Array characterRelated;
+    Array characterRelatedFail;
 
     @Before
     public void setUp() throws Exception {
@@ -34,11 +36,15 @@ public class InteractionsTest {
         characterRelated = new Array<>();
         characterRelated.add(0);
 
+        characterRelatedFail = new Array<>();
+        characterRelatedFail.add(99);
+
         murderMotive = new Clue(0, "murder motive", "murder motive description", ClueType.Motive, characterRelated, "motive response");
         murderWeapon = new Clue(1, "murder weapon", "murder weapon description", ClueType.Weapon, characterRelated, "weapon response");
         clueOne = new Clue(2, "clue one", "clue one description", ClueType.Appearance, characterRelated, "clue one response");
         clueTwo = new Clue(3, "clue two", "clue two description", ClueType.Appearance, characterRelated, "clue two response");
         clueThree = new Clue(4, "clue three", "clue three description", ClueType.Appearance, characterRelated, "clue three response");
+        failClue = new Clue(5, "clue four", "clue four description", ClueType.Appearance, characterRelatedFail, "clue four response");
 
         item = new Item(0, "test item", "test item description", 0, returnItem, false);
         returnItem = new Item(1, "return item", "return item description", true);
@@ -51,6 +57,7 @@ public class InteractionsTest {
         interactions = null;
         target = null;
         characterRelated = null;
+        characterRelatedFail = null;
         murderMotive = null;
         murderWeapon = null;
         clueOne = null;
@@ -85,8 +92,15 @@ public class InteractionsTest {
     }
 
     @Test
-    public void accuse() throws Exception {
+    public void accuseFailTest() throws Exception {
+        assertEquals("failure - returned statement is not correct", "you have accused the wrong person", interactions.accuse(target,murderMotive,murderWeapon,clueOne,clueTwo, failClue));
+        assertTrue("failure - accused status was not set to true", target.isAccused());
 
+    }
+
+    @Test
+    public void accuseSuccesTest() throws Exception {
+        assertEquals("failure - returned statement is not correct", "You have accused the correct person congratulations you win!", interactions.accuse(target, murderMotive, murderWeapon, clueOne, clueTwo, clueThree));
     }
 
     @Test
