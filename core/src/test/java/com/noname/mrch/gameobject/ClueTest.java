549d1eca8c02abcd8328ebc63f2d2116b0c30181
jpn506 <jpn506@york.ac.uk>
2016-11-29 12:53:53 +0000

    Clue updated with checkId(target) function

--- core/src/test/java/com/noname/mrch/gameobject/ClueTest.java
+++ core/src/test/java/com/noname/mrch/gameobject/ClueTest.java
@@ -15,41 +15,56 @@ import com.badlogic.gdx.utils.Array;
 public class ClueTest {
     Clue clue;
     Array<Integer> characterRelated;
+    GameCharacter succesfulTarget;
+    GameCharacter failingTarget;
 
     @Before
     public void setUp() throws Exception {
+        succesfulTarget = new GameCharacter(0,"test name", Personality.Aggressive, false, false, false, "hello test", "negative response test");
+        failingTarget = new GameCharacter(5, "test name", Personality.Aggressive, false, false, false, "hello", "negative test response");
+
+        characterRelated.add(0);
+        characterRelated.add(1);
+
         clue = new Clue(0,"test clue", "test description", Motive, characterRelated, "test response");
     }
 
     @After
     public void tearDown() throws Exception {
         clue = null;
+        succesfulTarget = null;
+        failingTarget = null;
         characterRelated = null;
     }
 
     @Test
-    public void getId() throws Exception {
+    public void getIdTest() throws Exception {
         assertEquals("failure - test id not returned", 0, clue.getId());
     }
 
     @Test
-    public void getDescription() throws Exception {
+    public void getDescriptionTest() throws Exception {
         assertEquals("failure - test description not returned", "test description", clue.getDescription());
     }
 
     @Test
-    public void getRelatedCharId() throws Exception {
+    public void getRelatedCharIdTest() throws Exception {
         assertEquals("failure - test character id not returned", characterRelated, clue.getRelatedCharId());
     }
 
     @Test
-    public void getClueType() throws Exception {
+    public void getClueTypeTest() throws Exception {
         assertEquals("failure - test clue type not returned", Motive, clue.getClueType());
     }
 
     @Test
-    public void getResponse() throws Exception {
+    public void getResponseTest() throws Exception {
         assertEquals("failure - test response not returned", "test response", clue.getResponse());
     }
 
+    @Test
+    public void checkIdTest() throws Exception {
+        assertEquals("failure - returned boolean was not correct", true, clue.checkId(succesfulTarget));
+        assertEquals("failure - returned boolean was not correct", false, clue.checkId(failingTarget));
+    }
 }
