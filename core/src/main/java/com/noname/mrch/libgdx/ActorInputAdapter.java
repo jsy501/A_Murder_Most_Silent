a0b87406464f679bd07dbb0f42848032e00f42ed
jpn506 <jpn506@york.ac.uk>
2016-11-30 13:50:17 +0000

    Actor input adapter finished

--- core/src/main/java/com/noname/mrch/libgdx/ActorInputAdapter.java
+++ core/src/main/java/com/noname/mrch/libgdx/ActorInputAdapter.java
@@ -2,6 +2,7 @@ package com.noname.mrch.libgdx;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.noname.mrch.Interactions;
 import com.noname.mrch.gameobject.*;
 
 /**
@@ -11,28 +12,44 @@ import com.noname.mrch.gameobject.*;
 public class ActorInputAdapter extends InputListener{
     GameActor actor;
     String selectedOption;
+    Room room;
+    String toGui;
+    Interactions interactions = Interactions.getInstance();
+    int questioningStle;
+
     public ActorInputAdapter(GameActor actor){
         this.actor = actor;
     }
     @Override
-    public void touchUp (InputEvent event, float x, float y, int pointer, int button){
+    public boolean touchUp (InputEvent event, float x, float y, int pointer, int button){
         if (this.actor instanceof Clue) {
             //todo gui displays some info
-            NoteBook.getInstance().addClue((Clue)this.actor);
-            (Room) actor.getStage().removeClue((Clue)this.actor);
-            this.actor.remove();
-        } else if (this.actor instanceof Item) {
+            NoteBook.getInstance().addClue((Clue) actor);
+            room =(Room) actor.getStage();
+            room.removeClue((Clue) actor);
+            actor.remove();
+        } else if (actor instanceof Item) {
             //todo gui displays some info
-            NoteBook.getInstance().addItem((Item)this.actor);
-            (Room) actor.getStage().removeItem((Item)this.actor);
-            this.actor.remove();
+            NoteBook.getInstance().addItem((Item) actor);
+            room = (Room) actor.getStage();
+            room.removeItem((Item) actor);
+            actor.remove();
         } else if (this.actor instanceof GameCharacter) {
             //todo gui displays options
+            selectedOption = "question"; //this is set by the gui option
+            questioningStle = 1; // provided by gui option
             if (selectedOption == "question") {
-                
+                toGui = interactions.question(questioningStle,(GameCharacter) actor);
+            } else if (selectedOption == "give") {
+                //todo add code to run the stuff
+            } else if (selectedOption == "accuse") {
+                //todo add code to run the stuff
+            } else if (selectedOption == "ignore") {
+                toGui = interactions.ignore();
             }
 
         }
+        return true;
     }
 }
 
