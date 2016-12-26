6290aa8e7baa09a51167feacc325d32f164d29c1
Jeff <jsy501@york.ac.uk>
2016-12-26 16:40:08 +0900

    Temporary graphics

--- core/src/main/java/com/noname/mrch/GUI/NoteBookWindow.java
+++ core/src/main/java/com/noname/mrch/GUI/NoteBookWindow.java
@@ -1,9 +1,23 @@
 package com.noname.mrch.gui;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
 import com.noname.mrch.GameWorld;
+import com.noname.mrch.gameobject.GameActor;
+import com.noname.mrch.gameobject.JsonImport;
+import com.noname.mrch.gameobject.NoteBook;
 
 public class NoteBookWindow extends GuiWindow {
+    private final float COLUMN_COUNT = 4;
+    private final float SLOT_WIDTH = 200;
+    private final float SLOT_GAP_HORIZONTAL = 100;
+    private final float SLOT_GAP_VERTICAL = SLOT_GAP_HORIZONTAL * 0.25f;
+    private final float WINDOW_WIDTH = SLOT_WIDTH * COLUMN_COUNT + SLOT_GAP_HORIZONTAL * (COLUMN_COUNT - 1) * 1.1f;
+    private final float WINDOW_HEIGHT = 700;
+
     public NoteBookWindow(Skin skin, Gui gui, GameWorld gameWorld) {
         super("NOTEBOOK", skin, gui, gameWorld);
 
@@ -13,14 +27,47 @@ public class NoteBookWindow extends GuiWindow {
         setModal(true);
     }
 
+    /**
+     * called every time notebook is opened. Clears all the children and rebuild.
+     */
+    public void refresh(){
+        NoteBook noteBook = gameWorld.getNotebook();
+        getContentTable().clear();
+        getContentTable().align(Align.topLeft);
+
+        Array<JsonImport> entryArray = new Array<>();
+        entryArray.addAll(noteBook.getClueList());
+        entryArray.addAll(noteBook.getItemList());
+
+        for (int i = 0; i < entryArray.size; i++){
+            Table slot = new Table(getSkin());
+            slot.add((GameActor)entryArray.get(i)).row();
+            slot.add(entryArray.get(i).getName()).row();
+
+            Label description = new Label(entryArray.get(i).getDescription(), getSkin());
+            description.setAlignment(Align.center);
+            description.setWrap(true);
+            slot.add(description).width(SLOT_WIDTH);
+
+            if ((i+1) % COLUMN_COUNT == 0){
+                getContentTable().add(slot).padTop(SLOT_GAP_VERTICAL)
+                        .padBottom(SLOT_GAP_VERTICAL).row();
+            }
+            else{
+                getContentTable().add(slot).padTop(SLOT_GAP_VERTICAL)
+                        .padBottom(SLOT_GAP_VERTICAL).padRight(SLOT_GAP_HORIZONTAL);
+            }
+        }
+    }
+
     @Override
     public float getPrefWidth() {
-        return 1500;
+        return WINDOW_WIDTH;
     }
 
     @Override
     public float getPrefHeight() {
-        return 700;
+        return WINDOW_HEIGHT;
     }
 
     @Override
